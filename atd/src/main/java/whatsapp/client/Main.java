57a01f2848c38905491a142b59b32a65eacd81ef
gennady9 <gennady9@gmail.com>
2019-06-06 00:25:46 +0300

    client connect impl

--- atd/src/main/java/whatsapp/client/Main.java
+++ atd/src/main/java/whatsapp/client/Main.java
@@ -1,34 +1,81 @@
 package whatsapp.client;
+// package internal imports
+import whatsapp.common.ConnectMessage;
 
-import java.io.IOException;
 
+// Akka imports
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
-// import akka.actor.ActorSelection;
 
-import whatsapp.client.User.Connect;
+import com.typesafe.config.ConfigFactory;
+// Java imports
+import java.util.Scanner;
+// import java.io.IOException;
+
 
 public class Main {
   public static void main(String[] args) {
-    final ActorSystem system = ActorSystem.create("userSystem");
+    final ActorSystem system = ActorSystem.create("client", ConfigFactory.load("user"));
     try {
       //#create-actors
       final ActorRef userActor = 
         system.actorOf(User.props(), "userActor");
 
-      // ActorSelection selection =
-        // context.actorSelection("akka.tcp://app@10.0.0.1:2552/user/serviceA/worker");
-      // selection.tell("Pretty awesome feature", getSelf());
+        // Scanner in or System.in.read?
+        Scanner scanner = new Scanner(System.in);
 
-      //#main-send-messages
-      userActor.tell(new Connect(), ActorRef.noSender());
+        String input;
+        while(true){
+            System.out.print(">");
+            input = scanner.nextLine();
+            if(input.startsWith("/user")){
+              handle_user_cmd(userActor, input);
+            }
+            // TODO: handle /group
+        }
 
-      System.out.println(">>> Terminal: <<<");
-      System.in.read();
-
-    } catch (IOException ioe) {
+    // } catch (IOException ioe) { // TODO: maybe i need to handle this?
     } finally {
       system.terminate();
     }
   }
+
+  private static void handle_user_cmd(ActorRef userActor, String input){
+    System.out.println("debug: [handle_user_cmd] input=" + input);
+    String[] input_array = input.split("\\s+");  // remove spaces and seperate
+    // input array structure:
+    // [0] /user, [1] command, [2] first param
+    String command = input_array[1];
+    String sec_param = input_array[2];
+    System.out.println("debug: [handle_user_cmd] command=" + command + " sec_param="+sec_param);
+    // handeling command
+    if      (command.equals("connect")){
+    System.out.println("debug: [handle_user_cmd] entered connect command case");
+
+      userActor.tell(new ConnectMessage(sec_param, ActorRef.noSender()), ActorRef.noSender()); // TODO: is it okay for connect message to receive nosender? maybe send getself?
+    }else if(command.equals("disconnect")){
+      userActor.tell(new disconnectMessage(), ActorRef.noSender());
+    // USER COMMUNICATION
+    }else if(command.equals("text")){
+    }else if(command.equals("file")){
+    }else{ // unknown command, error? what to do in this case..
+    }
+  }
+
+  // Behaviours:
+  public static class disconnectMessage{}
 }
+
+
+
+
+
+
+// Code graveyard R.I.P
+  /*
+  public static class connectMessage{ // Moved to common folder, delete if won't be neccesery.
+    String username;
+    public connectMessage(String username){
+      this.username = username;
+    }
+  }*/
--- atd/src/main/java/whatsapp/client/User.java
+++ atd/src/main/java/whatsapp/client/User.java
@@ -1,45 +1,86 @@
 package whatsapp.client;
 
+// Package internal imports
+import whatsapp.common.ConnectMessage;
+import whatsapp.server.ManagingServer;
+// Akka imports
 import akka.actor.AbstractActor;
-// import akka.actor.ActorRef;
+import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.actor.ActorSelection;
+import akka.pattern.Patterns;
+import akka.util.Timeout;
+import scala.concurrent.duration.Duration;
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 
-//#User-messages
+import java.io.Serializable;
+// Java imports
+import java.util.HashMap;
+// import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+
 public class User extends AbstractActor {
-//#User-messages
+
+  // User actor fields
+  String username;
+  // HashMap<String, ActorRef> groups = new HashMap<String, ActorRef>();
+  final ActorSelection managerServer = 
+    getContext().actorSelection("akka.tcp://server@127.0.0.1:3553/user/server");
+    // getContext().actorSelection("akka://server@127.0.0.1:3553/user/server");
+    // getContext().actorSelection("akka://whatsapp@127.0.0.1:3553/user/server");
+    
+  final static Timeout timeout_time = new Timeout(Duration.create(2, TimeUnit.SECONDS));
+  LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+
+// Props
   static public Props props(/*String userName, ActorRef serverActor*/) {
-    return Props.create(User.class, () -> new User(/*"userName", serverActor*/));
+    return Props.create(User.class, () -> new User()); // TODO: new User or ChatActor::new
   }
 
-
-  static public class Connect {
-    public Connect() {
+  // Behaviours / "Methods":
+  private void connectUser(String username){// TODO: Area51
+    Future<Object> future = Patterns.ask(managerServer, new ConnectMessage(username, getSelf()), timeout_time); 
+    try {
+      Object res = Await.result(future, timeout_time.duration());
+      if(res instanceof connectSuccess){
+        log.info("Connection successed!");
+        this.username = username;
+      }else{
+        // TODO: handle username is taken error
+        log.info("Connection failed!"); // TODO: maybe change to message?
+      }
+    }catch(Exception error){ // Catch error -> Server is offline
+      log.info("“server is offline! error= " + error);
     }
   }
-  //#user-messages
-//   private final String userName;
-//   private final ActorRef serverActor;
 
-  public User(/*String userName, ActorRef serverActor*/) {
-    // this.userName = userName;
-    // this.serverActor = serverActor;
-  }
 
+ public static class connectSuccess implements Serializable{
+   String message;
+   public connectSuccess(String message){
+     this.message = message;
+   }
+ }
   
   @Override
   public Receive createReceive() {
     return receiveBuilder()
-  //       .match(Connect.class, x -> {
-  //         //#connect-send-message
-  //         System.out.println("matched with connect");
-  //         ActorSelection selection =
-  //         getContext().actorSelection("akka.tcp://app@10.0.0.1:2552/whatsapp/server/Server");            
-  //         selection.tell(new connectUser("test", getSelf()), getSelf());
-  //         //#connect-send-message
-  //       })
+        .match(ConnectMessage.class, x -> connectUser(x.getUsername()))
+        // .matchAny(apply) // TODO: add matchAny?
         .build();
   }
 
 }
 
+
+
+// Code graveyard R.I.P
+
+ // public User(/*String userName, ActorRef serverActor*/) { // TODO: probably not needed
+    // this.userName = userName;
+    // this.serverActor = serverActor;
+ // }
